224cd18e6c78fce8d08bf5827b7900e470bfe4d0
Drew Baumann <drewbaumann@gmail.com>
2013-04-23 15:18:38 -0700

    Since this is a module and included in api I needed to change the way I was calling the verb methods.

--- lib/mirror-api/timeline_request.rb
+++ lib/mirror-api/timeline_request.rb
@@ -4,7 +4,7 @@ module Mirror
   module TimelineRequest
     def insert_timeline(params)
       begin
-        result = API.new(params: params, expected_response: 200, invoke_url: "#{Mirror.endpoint}/timeline").post
+        result = post({params: params, invoke_url: "#{Mirror.endpoint}/timeline"})
         Timeline.new(result) unless result.nil?
       rescue => e
         raise e
@@ -15,7 +15,7 @@ module Mirror
       parameters = params.map {|param| "#{param[0]}=#{param[1]}"}.join('&')
       encoded_parameters = URI.encode(parameters)
       invoke_url = encoded_parameters.empty? ? "#{Mirror.endpoint}/timeline" : "#{Mirror.endpoint}/timeline?#{encoded_parameters}"
-      result = API.new(expected_response: 200, invoke_url: "#{Mirror.endpoint}/timeline").get
+      result = get({invoke_url: "#{Mirror.endpoint}/timeline"})
       timeline_array = TimelineArray.new(result['nextPageToken'])
       result['items'].each {|item| timeline_array << Timeline.new(item) }
       timeline_array
