155d7e241f4c4532250931b29ea4c6ed78b8c50c
Drew Baumann <drewbaumann@gmail.com>
2013-04-23 15:19:16 -0700

    updated specs to handle authentication and the modified methods.

--- spec/timeline_spec.rb
+++ spec/timeline_spec.rb
@@ -1,67 +1,73 @@
 require_relative "spec_helper"
 
-describe Mirror::Timeline do
+describe Mirror::API do
   before do
+    stub_request(:post, "https://accounts.google.com/o/oauth2/token").
+         with(:body => {"client_id"=>"client_id", "client_secret"=>"client_secret", "grant_type"=>"refresh_token", "refresh_token"=>"1/asdasdasdasdas-asdasdasdasdasdas"},
+              :headers => {'Accept'=>'*/*', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Ruby'}).
+         to_return(:status => 200, :body => {access_token: 'access_token_for_testing'}.to_json, :headers => {})
     Mirror.configure do |config|
-      config.refresh_token = "refresh_token"
       config.client_secret  = "client_secret"
       config.client_id  = "client_id"
-      config.access_token = "access_token"
     end
+    @client = Mirror::API.new(refresh_token: '1/asdasdasdasdas-asdasdasdasdasdas')
   end
 
-  describe "insert" do
-    describe "inserting" do
-
-     context "with valid params" do
-       before do
-         @msg = "Hello world"
-         @body = {text: @msg}.to_json
-         stub_request(:post, "https://www.googleapis.com/mirror/v1/timeline").
-             with(body: @body,
-                  headers: json_post_request_headers(@body.length)).
-             to_return(status: 200,
-                       body: fixture("timeline_item.json", true),
-                       headers: JSON.parse(fixture("timeline_item_response_headers.json", true)))
-       end
-
-       it "should insert plain text items" do
-         item = Mirror::Timeline.insert({text: @msg})
-         item.should_not be_nil
-         item.created.should == "2012-09-25T23:28:43.192Z"  # see fixture
-         item.text.should == @msg
-       end
+  describe "insert_timeline" do
+    
+   context "with valid params" do
+     before do
+       @msg = "Hello world"
+       @body = {text: @msg}.to_json
+       stub_request(:post, "https://www.googleapis.com/mirror/v1/timeline").
+           with(body: @body,
+                headers: json_post_request_headers(@body.length)).
+           to_return(status: 200,
+                     body: fixture("timeline_item.json", true),
+                     headers: JSON.parse(fixture("timeline_item_response_headers.json", true)))
      end
 
-     context "with invalid params" do
-       before do
-         @msg = "123"
-         @body = {random: @msg}.to_json
-         # TODO: Verify error code is 422
-         stub_request(:post, "https://www.googleapis.com/mirror/v1/timeline").
-             with(body: @body,
-                  headers: json_post_request_headers(@body.length)).
-             to_return(status: 422, body: {}.to_json,
-                       headers: {})
-       end
-
-       it "should not insert the item" do
-         item = Mirror::Timeline.insert({random: "123"})
-         item.should be_nil
-       end
+     it "should insert plain text items" do
+       item = @client.insert_timeline({text: @msg})
+       item.should_not be_nil
+       item.created.should == "2012-09-25T23:28:43.192Z"  # see fixture
+       item.text.should == @msg
      end
+   end
+
+   context "with invalid params" do
+     before do
+       @msg = "123"
+       @body = {random: @msg}.to_json
+       # TODO: Verify error code is 422
+       stub_request(:post, "https://www.googleapis.com/mirror/v1/timeline").
+           with(body: @body,
+                headers: json_post_request_headers(@body.length)).
+           to_return(status: 422, body: {}.to_json,
+                     headers: {})
+     end
+
+     it "should not insert the item" do
+       item = @client.insert_timeline({random: "123"})
+       item.should be_nil
+     end
+   
     end
 
     def json_post_request_headers(length)
       {
           'Accept'=>'application/json',
           'Accept-Encoding'=>'gzip, deflate',
-          'Authorization'=>"Bearer #{Mirror.access_token}",
+          'Authorization'=>"Bearer #{@client.access_token}",
           'Content-Length'=>length.to_s,
           'Content-Type'=>'application/json',
           'User-Agent'=>'Ruby'
       }
     end
+  end
+
+  describe "list" do
+    context
   end
 
 end
