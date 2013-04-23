90ba4a53ad38c8675e6744825f8064c49230713c
Drew Baumann <drewbaumann@gmail.com>
2013-04-23 16:34:45 -0700

    Made nextPageToken readable so that the app can access it to call other pages!

--- lib/mirror-api/timeline_array.rb
+++ lib/mirror-api/timeline_array.rb
@@ -2,6 +2,7 @@ require 'forwardable'
 class TimelineArray
   include Enumerable
   extend Forwardable
+  attr_reader :nextPageToken
   def_delegators :@array, :each, :<<
   def initialize(nextPageToken=nil)
     @nextPageToken = nextPageToken
