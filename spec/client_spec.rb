d7dd9bc591bd58d3bd36733f78feae2f2b68cb02
Drew Baumann <drewbaumann@gmail.com>
2013-04-24 02:55:28 -0700

    added specs for location

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -37,7 +37,7 @@ describe Mirror::Api::Client do
            # TODO: Verify error code is 422
            stub_request(:post, "https://www.googleapis.com/mirror/v1/timeline/").
                with(body: {random: "123"},
-                    headers: json_post_request_headers).
+                    headers: json_get_request_headers).
                to_return(status: 422, body: {}.to_json,
                          headers: {})
          end
@@ -47,19 +47,94 @@ describe Mirror::Api::Client do
            item = @api.timeline.create({random: "123"})
            item.should be_nil
          end
-       end
-      end
 
-      def json_post_request_headers
-        {
-            'Accept'=>'application/json',
-            'Accept-Encoding'=>'gzip, deflate',
-            'Authorization'=>"Bearer #{@token}",
-            'Content-Length'=>/\d+/,
-            'Content-Type'=>'application/x-www-form-urlencoded',
-            'User-Agent'=>'Ruby'
-        }
+       end
+
       end
     end
   end
+
+  describe "locations" do
+   
+    describe "get" do
+
+      context "with valid params" do
+        before do
+          @id = "0987"
+
+          stub_request(:get, "https://www.googleapis.com/mirror/v1/locations/#{@id}").
+                  with(headers: json_get_request_headers).
+             to_return(status: 200,
+                       body: fixture("locations_item.json", true),
+                       headers: {})
+        end
+
+      it "should get the location for @id", :focus => true do
+        location = @api.locations.get(@id)
+        location.should_not be_nil
+        location.displayName.should == "Home"  # see fixture
+      end
+    end
+
+    context "with invalid params" do
+      before do
+        @id = "0987asdasds"
+
+        # TODO: Verify error code is 422
+        stub_request(:get, "https://www.googleapis.com/mirror/v1/locations/").
+          with(headers: json_post_request_headers).
+            to_return(status: 422, body: {}.to_json,
+                       headers: {})
+        end
+
+        it "should not get the item" do
+
+          item = @api.timeline.create({random: "123"})
+          item.should be_nil
+        end
+      end
+    end 
+    describe "list" do
+
+      context "with valid params" do
+        before do
+
+          stub_request(:get, "https://www.googleapis.com/mirror/v1/locations/").
+                  with(headers: json_get_request_headers).
+                    to_return(status: 200,
+                       body: fixture("locations_list.json", true),
+                       headers: {})
+        end
+
+        it "should return a list of ids", :focus => true do
+          locations = @api.locations.list()
+          locations.should_not be_nil
+          locations.items.count.should == 2  # see fixture
+        end
+      end
+    end
+  end
+
+
+  def json_post_request_headers
+    {
+        'Accept'=>'application/json',
+        'Accept-Encoding'=>'gzip, deflate',
+        'Authorization'=>"Bearer #{@token}",
+        'Content-Length'=>/\d+/,
+        'Content-Type'=>'application/x-www-form-urlencoded',
+        'User-Agent'=>'Ruby'
+    }
+  end
+
+  def json_get_request_headers
+    {
+        'Accept'=>'application/json',
+        'Accept-Encoding'=>'gzip, deflate',
+        'Authorization'=>"Bearer #{@token}",
+        'Content-Type'=>'application/json',
+        'User-Agent'=>'Ruby'
+    }
+  end
+
 end
