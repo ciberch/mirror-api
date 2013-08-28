aded1a6645f046d85f8b697fb6229a3f3700946d
Monica Wilkinson <monica@crushpath.com>
2013-08-27 19:19:55 -0700

    Fixes #45

--- lib/mirror-api/errors.rb
+++ lib/mirror-api/errors.rb
@@ -1,9 +1,12 @@
+require_relative "not_found_error"
+require_relative "bad_request_error"
+
 module Mirror
   module Api
     class Errors
-      ERROR_400 = {code: 1, :message => "Mirror API returned a status code 400 for this call"}
-      ERROR_404 = {code: 2, :message => "Mirror API returned a status code 404 for this call"}
-      INTERNAL_ERROR = {:code => 100, :message => "We are experiencing problems requesting your name. Our team has been notified."}
+      ERROR_400 = {code: 1, :message => "Mirror API returned a status code 400 for this call", :ex_class => Mirror::Api::BadRequestError}
+      ERROR_404 = {code: 2, :message => "Mirror API returned a status code 404 for this call", :ex_class => Mirror::Api::NotFoundError}
+      ERROR = {:code => 100, :message => "Mirror API returned an unexpected status code"}
       TOKEN_REFRESH_ERROR = {:code => 200, :message => "Could not refresh your API access token"}
     end
 end
